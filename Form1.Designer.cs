bbc68dad4da44c7745e831694d57a2509cbd7ea4
mamzerdevlad@gmail.com <mamzerdevlad@gmail.com>
2022-06-15 03:05:02 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace SnakaGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBoxField = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(0, 743);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1584, 118);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBoxField
+            // 
+            this.pictureBoxField.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxField.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxField.Name = "pictureBoxField";
+            this.pictureBoxField.Size = new System.Drawing.Size(1584, 743);
+            this.pictureBoxField.TabIndex = 1;
+            this.pictureBoxField.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.pictureBoxField);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(720, 640);
+            this.Name = "Form1";
+            this.Text = "SnakeGame";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxField)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBoxField;
+    }
+}
+
---
+++ __SNAKE.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakaGame
+{
+    enum SnakePartName
+    {
+        Head = 1,
+        Body,
+        Tail,
+        Apple
+    }
+    class SnakePart
+    {
+        public bool IsEaten;
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Angle { get; set; }
+        public SnakePart(SnakePartName Part, int X, int Y)
+        {
+            //this.Part = Part;
+            this.X = X;
+            this.Y = Y;
+            if (Part == SnakePartName.Head)
+            {
+                Angle = 1;
+            }
+            if (Part == SnakePartName.Apple)
+            {
+                IsEaten = false;
+            }
+        }
+    }
+    class SNAKE
+    {
+        public bool IsAlive = true;
+        public SnakePart HEAD;
+        public SnakePart[] BODY;
+        public SnakePart TAIL;
+        public SnakePart Apple;
+        public int length = 1;
+        int MAX_X, MAX_Y;
+        public SNAKE(int MAX_X, int MAX_Y)
+        {
+            this.MAX_X = MAX_X;
+            this.MAX_Y = MAX_Y;
+            BODY = new SnakePart[length];
+            HEAD = new SnakePart(SnakePartName.Head, MAX_X / 2, MAX_Y / 2);
+            BODY[0] = new SnakePart(SnakePartName.Body, MAX_X / 2, (MAX_Y / 2) + 1);
+            TAIL = new SnakePart(SnakePartName.Tail, MAX_X / 2, (MAX_Y / 2) + 2);
+            Apple = new SnakePart(SnakePartName.Apple, 0, 0);
+        }
+        public void Step()
+        {
+            for (int _i = 0; _i < length; _i++)
+            {
+                if (HEAD.Angle == GetPartAngle(BODY[_i], HEAD))
+                {
+                    IsAlive = false;
+                    return;
+                }
+                if (_i == 0 && HEAD.Angle == GetPartAngle(TAIL, HEAD))
+                {
+                    IsAlive = false;
+                    return;
+                }
+            }
+            if (HEAD.Angle == GetPartAngle(Apple, HEAD))
+            {
+                Apple.IsEaten = true;
+                length++;
+                SnakePart[] _BODY = new SnakePart[length];
+                _BODY[0] = new SnakePart(SnakePartName.Body, HEAD.X, HEAD.Y);
+                for (int _i = 1; _i < length; _i++)
+                {
+                    _BODY[_i] = new SnakePart(SnakePartName.Body, BODY[_i - 1].X, BODY[_i - 1].Y);
+                }
+                BODY = _BODY;
+                MakeStep(HEAD, HEAD.Angle);
+            }
+            else
+            {
+                MakeStep(TAIL, GetPartAngle(BODY[length - 1], TAIL));
+                for (int _i = length - 1; _i >= 0; _i--)
+                {
+                    if (_i == 0)
+                    {
+                        MakeStep(BODY[_i], GetPartAngle(HEAD, BODY[_i]));
+                    }
+                    else
+                    {
+                        MakeStep(BODY[_i], GetPartAngle(BODY[_i - 1], BODY[_i]));
+                    }
+                }
+                MakeStep(HEAD, HEAD.Angle);
+            }
+        }
+        void MakeStep(SnakePart Part, int angle)
+        {
+            switch (angle)
+            {
+                case 0:
+                    Part.X = (Part.X + MAX_X + 1) % MAX_X;
+                    break;
+                case 1:
+                    Part.Y = (Part.Y + MAX_Y - 1) % MAX_Y;
+                    break;
+                case 2:
+                    Part.X = (Part.X + MAX_X - 1) % MAX_X;
+                    break;
+                case 3:
+                    Part.Y = (Part.Y + MAX_Y + 1) % MAX_Y;
+                    break;
+            }
+        }
+        int GetPartAngle(SnakePart X, SnakePart Y)         //Y - центральный объект, X - целевой объект
+        {
+            int num;
+            if (X.X == (Y.X + MAX_X + 1) % MAX_X && X.Y == Y.Y) num = 0;     //Справа
+            else if (X.X == Y.X && X.Y == (Y.Y + MAX_Y - 1) % MAX_Y) num = 1;//Сверху
+            else if (X.X == (Y.X + MAX_X - 1) % MAX_X && X.Y == Y.Y) num = 2;//Слева
+            else if (X.X == Y.X && X.Y == (Y.Y + MAX_Y + 1) % MAX_Y) num = 3;//Снизу
+            else num = -1;                                                   //Не найдено
+            return num;
+        }
+        public void SetApplePosition(int[] Coordinates)
+        {
+            Apple.X = Coordinates[0];
+            Apple.Y = Coordinates[1];
+        }
+    }
+}
